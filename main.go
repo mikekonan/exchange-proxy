4ca68cf8daa80763b4f92bf50111199ae4a39df6
Mikalai Konan <mikalai.konan@icloud.com>
2021-12-04 22:58:01 +0300

    more topics, verbosity, docker image

--- main.go
+++ main.go
@@ -10,15 +10,22 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func init() {
-	logrus.SetOutput(os.Stdout)
-	logrus.SetLevel(logrus.DebugLevel)
-	logrus.AddHook(logrus_stack.StandardHook())
-}
-
 func main() {
+	logrus.SetOutput(os.Stdout)
+	logrus.AddHook(logrus_stack.StandardHook())
+
 	port := flag.Int("port", 8080, "listen port")
+	verbose := flag.Int("verbose", 0, "verbose level. 0 - info [default]. 1 - debug. 2 - trace.")
 	flag.Parse()
+
+	switch *verbose {
+	case 0:
+		logrus.SetLevel(logrus.InfoLevel)
+	case 1:
+		logrus.SetLevel(logrus.DebugLevel)
+	case 2:
+		logrus.SetLevel(logrus.TraceLevel)
+	}
 
 	s := store.New()
 	k := kucoin.New(s)
